c792964349ec17f820ad154b7fc4dbbf11a1c3b0
barbarajg <basjgamboa@gmail.com>
2023-02-23 21:23:21 +0100

    Close cart on backdrop

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,16 +1,17 @@
-import classes from './Modal.module.css';
 import ReactDOM from 'react-dom';
 
+import classes from './Modal.module.css';
+
 const Backdrop = (props) => {
-    return <div className={classes.backdrop} />;
+  return <div className={classes.backdrop} onClick={props.onClose}/>;
 };
 
 const ModalOverlay = (props) => {
-    return (
-        <div className={classes.modal}>
-            <div className={classes.content}>{props.children}</div>
-        </div>
-    );
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
 };
 
 const portalElement = document.getElementById('overlays');
@@ -18,8 +19,11 @@ const portalElement = document.getElementById('overlays');
 const Modal = (props) => {
   return (
     <>
-        {ReactDOM.createPortal(<Backdrop onCloseCart={props.onCloseCart} />, portalElement)}
-        {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
     </>
   );
 };
